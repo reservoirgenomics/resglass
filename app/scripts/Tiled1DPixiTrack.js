c0ccae9c35e866a5c897b4a396e66688871a7b62
Peter Kerpedjiev <pkerpedjiev@gmail.com>
2024-06-22 22:49:10 -0700

    Use comma for splitting cos

--- app/scripts/Tiled1DPixiTrack.js
+++ app/scripts/Tiled1DPixiTrack.js
@@ -71,7 +71,7 @@ class Tiled1DPixiTrack extends TiledPixiTrack {
      * The tile identifier used on the server
      */
     const remoteId = this.dataFetcher.dataConfig.chromOrderSource
-      ? `${tile.join('.')}#cos:${this.dataFetcher.dataConfig.chromOrderSource}`
+      ? `${tile.join('.')},cos:${this.dataFetcher.dataConfig.chromOrderSource}`
       : `${tile.join('.')}`;
 
     // tile contains [zoomLevel, xPos]
